5a054fb4928ab3db4c6b5e2163ff859e92c1929a
Ben <benhv@stud.ntnu.no>
2025-04-12 13:50:17 +0200

    refactor and global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,25 @@
   let { children } = $props();
 </script>
 
-<div class="nav">
+<div class="header">
+  <div>
+    <span>BUDGETTALISERING</span>
+  </div>
   {#if page.url.pathname !== "/"}
-    <a href="/">Home</a>
-    <a href="/expenses">Expenses</a>
-    <a href="/settings">Settings</a>
+    <nav class="nav-container">
+      <a href="/expenses">Utgifter</a>
+      <a href="/settings">TEMP</a>
+    </nav>
   {/if}
 </div>
 
 <main>
   {@render children()}
 </main>
+
+<style>
+  :global(body) {
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: #fef9f2;
+  }
+</style>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import ExpenseForm from "$lib/components/ExpenseForm.svelte";
+</script>
+
+<div>
+  <ExpenseForm />
+</div>
