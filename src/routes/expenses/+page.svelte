df95252adc17049b23aa7d3a82f14c7d5b3c8f05
Ben <benhv@stud.ntnu.no>
2025-05-01 00:01:50 +0200

    add basic filtration for category and calculate total forbruk

--- src/routes/expenses/+page.svelte
+++ src/routes/expenses/+page.svelte
@@ -1,12 +1,44 @@
-<script>
+<script lang="ts">
   import ExpenseList from "$lib/components/ExpenseList.svelte";
+  import { categories } from "$lib/utils/categories.js";
 
   let { data } = $props();
+
+  let selectedCategory: string | null = $state(null);
+
+  const filterCategory = (category: string) => {
+    selectedCategory = selectedCategory === category ? null : category;
+  };
+
+  const filterExpenses = $derived(
+    selectedCategory
+      ? data.expenses.filter((expense) => expense.category === selectedCategory)
+      : data.expenses
+  );
+
+  const totalExpenses = $derived(
+    filterExpenses.reduce((sum, expense) => sum + expense.amount, 0)
+  );
 </script>
 
 <div class="container">
   <h1 class="header">Oversikt over forbruk</h1>
-  <ExpenseList expenses={data.expenses} />
+  <div>
+    {#each categories as category}
+      <label>
+        <input
+          type="checkbox"
+          checked={selectedCategory === category}
+          onchange={() => filterCategory(category)}
+        />
+        {category}
+      </label>
+    {/each}
+  </div>
+  <ExpenseList expenses={filterExpenses} />
+  <span>
+    Total: {totalExpenses}
+  </span>
 </div>
 
 <style>
